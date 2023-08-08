8839e49ae799c2d0ec85f087e07d2975ef20ca91
KovaKreative <eric.kovalevskyy@gmail.com>
2023-08-08 13:46:55 -0400

    Database adjustments

--- openbook-server/db/seeds/01_users.sql
+++ openbook-server/db/seeds/01_users.sql
@@ -1,6 +1,6 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email, hash) VALUES ('Bram Stoker', 'blood.is.life@vamp.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (name, email, hash) VALUES ('Mary Shelley', 'marys@frank.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (name, email, hash) VALUES ('Edgar A. Poe', 'original.goth@nevermore.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (name, email, hash) VALUES ('Franz Kafka', 'fk@prague.cz', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (name, email, hash) VALUES ('Henry James', 'the.american@newyork.us', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, hash) VALUES ('Bram Stoker', 'bram@vamp.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, hash) VALUES ('Mary Shelley', 'mary@frank.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, hash) VALUES ('Edgar A. Poe', 'edgar@goth.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, hash) VALUES ('Franz Kafka', 'franz@metamorphosis.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, hash) VALUES ('Henry James', 'henry@american.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
